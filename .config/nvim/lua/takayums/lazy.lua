1ef35da5eda4db9bf92f0a510a15d6037b82dccf
TakayumAja <asraftakayuma76@gmail.com>
2023-10-06 19:13:55 +0700

    change importing specs lazynvim

--- .config/nvim/lua/takayums/lazy.lua
+++ .config/nvim/lua/takayums/lazy.lua
@@ -12,4 +12,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("takayums.plugins")
+-- require("lazy").setup("takayums.plugins")
+require("lazy").setup({
+  spec = {import = "takayums.plugins"}
+})
